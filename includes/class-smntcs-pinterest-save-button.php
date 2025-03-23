da84165416c6d534f2f1911e06cca6f9a2a75d27
Niels Lange <info@nielslange.de>
2025-03-23 14:29:16 +0700

    Update PHP CodeSniffer configuration and dependencies, enforcing stricter array syntax rules and enhancing action hook parameters in the Pinterest Save Button class constructor.

--- includes/class-smntcs-pinterest-save-button.php
+++ includes/class-smntcs-pinterest-save-button.php
@@ -17,7 +17,7 @@ class SMNTCS_Pinterest_Save_Button {
 	 */
 	public function __construct() {
 		add_action( 'customize_register', [ $this, 'smntcs_pinterest_save_button_register_customize' ] );
-		add_action( 'wp_footer', [ $this, 'smntcs_pinterest_save_button_enqueue_pinterest_script' ] );
+		add_action( 'wp_footer', [ $this, 'smntcs_pinterest_save_button_enqueue_pinterest_script' ], 10, 0 );
 	}
 
 	/**
